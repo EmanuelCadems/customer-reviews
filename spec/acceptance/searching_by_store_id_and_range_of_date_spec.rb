093c8b158c23688bc7d1cc77a7c690b0fa1a46be
Emanuel Alarcón <emanuel_cadems@hotmail.com>
2018-09-29 00:01:05 -0300

    Test searching by store_id & range of date

--- spec/acceptance/searching_by_store_id_and_range_of_date_spec.rb
+++ spec/acceptance/searching_by_store_id_and_range_of_date_spec.rb
@@ -6,21 +6,21 @@ resource 'V1::Reviews', prefix: '/v1' do
 
   before do
     Timecop.freeze(Time.now.last_month)
-    create_list(:review, 6, user_id: 1)
+    create_list(:review, 6, store_id: 1)
     Timecop.return
-    create_list(:review, 4, user_id: 1)
+    create_list(:review, 4, store_id: 1)
     header 'Authorization', "Token token=#{ENV['USER_API_KEY']}"
   end
 
   context 'matching' do
-    get '/v1/reviews?q[user_id_eq]=:user_id&q[created_at_gteq]=:from&' +
+    get '/v1/reviews?q[store_id_eq]=:store_id&q[created_at_gteq]=:from&' +
         'q[created_at_lteq]=:to' do
-      let(:user_id) { 1 }
-      let(:from)    { Date.today.beginning_of_month }
-      let(:to)      { Date.today.end_of_month }
+      let(:store_id) { 1 }
+      let(:from)     { Date.today.beginning_of_month }
+      let(:to)       { Date.today.end_of_month }
 
-      example_request 'search by user_id & range of date' do
-        explanation 'Searches a review by user_id and range of date'
+      example_request 'search by store_id & range of date' do
+        explanation 'Searches a review by store_id and range of date'
 
         expect(status).to eq(200)
 
@@ -32,13 +32,13 @@ resource 'V1::Reviews', prefix: '/v1' do
   end
 
   context 'no-matching' do
-    get '/v1/reviews?q[user_id_eq]=:user_id&q[created_at_gteq]=:from&' +
+    get '/v1/reviews?q[store_id_eq]=:store_id&q[created_at_gteq]=:from&' +
         'q[created_at_lteq]=:to' do
-      let(:user_id) { 2 }
-      let(:from)    { Date.today.beginning_of_month }
-      let(:to)      { Date.today.end_of_month }
+      let(:store_id) { 2 }
+      let(:from)     { Date.today.beginning_of_month }
+      let(:to)       { Date.today.end_of_month }
 
-      example_request 'search by non-existent user_id & range of date' do
+      example_request 'search by non-existent store_id & range of date' do
         explanation 'Shows 204'
 
         expect(status).to eq(204)
