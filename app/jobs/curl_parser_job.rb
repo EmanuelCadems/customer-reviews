547aad0dc7be77cfd7cf74a6ac01f2532e8e58e3
Emanuel Alarcón <emanuel_cadems@hotmail.com>
2018-10-01 04:09:49 -0300

    Call to CurlRunnerJob inside CurlParserJob with each curl_cmd obtained from CurlParser

--- app/jobs/curl_parser_job.rb
+++ app/jobs/curl_parser_job.rb
@@ -3,5 +3,9 @@ class CurlParserJob < ApplicationJob
 
   def perform(review, execute_after)
     result = CurlParser.new(review.attributes, execute_after).exec
+
+    result.each do |curl_cmd|
+      CurlRunnerJob.perform_later curl_cmd
+    end
   end
 end
--- spec/jobs/curl_parser_job_spec.rb
+++ spec/jobs/curl_parser_job_spec.rb
@@ -6,6 +6,23 @@ RSpec.describe CurlParserJob, type: :job do
 
   let(:review) { create(:review, store_id: 2460286, opinion: 'dev') }
 
+  let(:curl_cmd) do
+    <<-EOF
+      curl --header "Content-Type: application/json" \
+         --request POST \
+         --data '{
+            "store_id": "#{review.store_id}",
+            "order_id": "#{review.order_id}",
+            "user_id": "#{review.user_id}",
+            "opinion": "#{review.opinion}",
+            "score": "#{review.score}",
+            "created_at": "#{review.created_at.iso8601(3)}",
+            "updated_at": "#{review.updated_at.iso8601(3)}"
+          }' \
+         https://api.jsonbin.io/b
+    EOF
+  end
+
   before do
     allow(CurlParser).to receive(:new)
                            .with(review.attributes, 'creating')
@@ -13,7 +30,7 @@ RSpec.describe CurlParserJob, type: :job do
                              CurlParser.new(review.attributes, 'creating')
                            )
     allow(CurlParser.new(review.attributes, 'creating'))
-      .to receive(:exec).and_return([])
+      .to receive(:exec).and_return([curl_cmd])
 
   end
 
@@ -26,6 +43,11 @@ RSpec.describe CurlParserJob, type: :job do
   it 'calls and executes CurlParser service' do
     expect(CurlParser).to receive(:new).with(review.attributes, 'creating')
     expect(CurlParser.new(review.attributes, 'creating')).to receive(:exec)
+    subject.perform(review, 'creating')
+  end
+
+  it 'calls CurlRunnerJob with each command obtained from the CurlParser' do
+    expect(CurlRunnerJob).to receive(:perform_later).with(curl_cmd)
     subject.perform(review, 'creating')
   end
 end
