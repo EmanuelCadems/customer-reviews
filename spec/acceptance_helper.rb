c1192627ec0dad1e316cae2fcfb9e711aacea1af
Emanuel Alarcón <emanuel_cadems@hotmail.com>
2018-10-01 05:12:39 -0300

    Add heroku endpoint in doc. Generate doc

--- spec/acceptance_helper.rb
+++ spec/acceptance_helper.rb
@@ -4,7 +4,7 @@ require 'rspec_api_documentation/dsl'
 
 RspecApiDocumentation.configure do |config|
   config.format = %i[JSON combined_text html]
-  config.curl_host = 'localhost:3000'
+  config.curl_host = 'https://stark-mesa-90873.herokuapp.com'
   config.api_name = 'Customer Reviews'
   config.api_explanation = 'This API allows you to manage customer reviews ' +
                            'inside different stores and orders'
